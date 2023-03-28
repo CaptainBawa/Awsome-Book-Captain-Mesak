204dacdabadd2fdb9bf944dab1bffde324af4671
Collins Bawa <collinskwamebawa@yahoo.com>
2023-03-28 15:12:54 +0000

    Add funtion to to get data from localStorage and display on the page

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const booksForm = document.getElementById('book-form');
 const booksList = document.getElementById('book-list');
 
 /* Creating an empty array. */
-const myBooks = [];
+let myBooks = [];
 
 /*
  The Book function is a constructor function that creates objects with two properties, title and
@@ -15,12 +15,6 @@ function Book(title, author) {
   this.title = title;
   this.author = author;
 }
-
-/* This is a method that is being added to the Book prototype.
-It is a function that returns a string
-that is the title and author of the book.
-*/
-Book.prototype.displayInfo = () => `${this.title.value} by ${this.author.value} `;
 
 /*
  It loops through the array of books, creates an HTML element for each one, and adds a
@@ -35,7 +29,13 @@ button to each element. */
   myBooks.forEach((book, index) => {
   /* Creating a list item and adding the book's title and author to it. */
     const li = document.createElement('li');
-    li.textContent = book.displayInfo();
+    /* Checking to see if the book has a displayInfo method. If it does, it calls the method.
+    If it doesn't, it displays the title and author. */
+    if (typeof book.displayInfo === 'function') {
+      li.textContent = book;
+    } else {
+      li.textContent = `${book.title} by ${book.author}`;
+    }
     li.style.listStyle = 'none';
 
     // Add a remove button for each book
@@ -71,6 +71,23 @@ function addBook(title, author) {
   saveBookToLocalStorage();
   displayBook();
 }
+
+/*
+ If there are books in local storage, parse them and store them in the myBooks array.
+ If there are no books in local storage, display a message to the user
+ */
+function getBooksFromLocalStorage() {
+  const books = localStorage.getItem('books');
+  if (books) {
+    myBooks = JSON.parse(books);
+  } else {
+    myBooks.innerHTML = 'No books found';
+  }
+}
+// Call the function to retrieve the list of books from local storage
+getBooksFromLocalStorage();
+// Call the displayBook function here to display the list of books on the page.
+displayBook();
 
 /* Adding an event listener to the form that listens for a submit event.
 When the event is triggered, it prevents the form from submitting and reloading the page.
