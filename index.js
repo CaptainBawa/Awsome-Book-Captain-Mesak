3382698e30f22b1a76085fe9fea4cfd212553a3f
MesakDuduCoder <duduaoa99@gmail.com>
2023-03-29 23:15:28 +0530

    Add Classes for Add and Remove Book

--- index.js
+++ index.js
@@ -1,110 +1,113 @@
-/* Creating a variable that is equal to the element with the id of book-form and book-list. */
-const booksForm = document.getElementById('book-form');
-const booksList = document.getElementById('book-list');
+// Book Class: Represents a Book
 
-/* Creating an empty array. */
-let myBooks = [];
+// UI Class: Handle UI Tasks
+class AddBook {
+  constructor(title, author, bookId) {
+    this.title = title;
+    this.author = author;
+    this.bookId = bookId;
+  }
 
-/*
- The Book function is a constructor function that creates objects with two properties, title and
- author.
- title - The title of the book.
- author - The author of the book.
- */
-function Book(title, author) {
-  this.title = title;
-  this.author = author;
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  }
+
+  static addBook(title, author, id) {
+    let idExist = false;
+    const books = AddBook.getBooks();
+    books.forEach((item) => {
+      if (item.bookID === id) {
+        idExist = true;
+        const getError = document.getElementById('error');
+        getError.textContent = 'Opps try agaian';
+      }
+    });
+
+    if (idExist === false) {
+      const book = new AddBook(title, author, id);
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(books));
+      document.querySelector('#title').value = '';
+      document.querySelector('#author').value = '';
+      document.location.reload();
+    }
+  }
 }
 
-/*
- It loops through the array of books, creates an HTML element for each one, and adds a
- remove button to each element
- */
-function displayBook() {
-  // Clearing the existing list.
-  booksList.innerHTML = '';
+class RemoveBook {
+  static removeBook(id) {
+    const bookID = parseInt(id, 10);
+    const books = AddBook.getBooks();
+    books.forEach((book, index) => {
+      if (book.bookId === bookID) {
+        books.splice(index, 1);
+        localStorage.setItem('books', JSON.stringify(books));
+        document.location.reload();
+      }
+    });
+  }
+}
 
-  /* Looping through the array of books, creating an HTML element for each one, and adding a remove
-button to each element. */
-  myBooks.forEach((book, index) => {
-  /* Creating a list item and adding the book's title and author to it. */
+// Display books
+function displayBooks() {
+  const books = AddBook.getBooks();
+  books.forEach((book) => {
+    const list = document.querySelector('#book-list');
     const li = document.createElement('li');
-    /* Checking to see if the book has a displayInfo method. If it does, it calls the method.
-    If it doesn't, it displays the title and author. */
-    if (typeof book.displayInfo === 'function') {
-      li.textContent = book;
-    } else {
-      li.textContent = `${book.title} by ${book.author}`;
-    }
-    li.style.listStyle = 'none';
-
-    // Add a remove button for each book
+    li.className = 'book-lists';
+    li.innerHTML = `"${book.title}" by ${book.author}`;
+    const gap = document.createElement('span');
+    gap.className = 'uniqueId';
+    gap.innerHTML = `${book.bookId}`;
     const removeButton = document.createElement('button');
     removeButton.textContent = 'Remove';
-    removeButton.addEventListener('click', () => {
-    /* Removing the book from the array and re-displaying the list. */
-      myBooks.splice(index, 1);
-      localStorage.setItem('books', JSON.stringify(myBooks));
-      displayBook();
-    });
-    /* Adding the remove button to the list item. */
+    removeButton.className = 'remove-btn';
+    li.prepend(gap);
     li.appendChild(removeButton);
-
-    // Add the book element to the list
-    booksList.appendChild(li);
+    list.prepend(li);
   });
 }
 
-// Save the books array to local storage as a string.
-function saveBookToLocalStorage() {
-  localStorage.setItem('books', JSON.stringify(myBooks));
-}
+document.addEventListener('DOMContentLoaded', displayBooks());
 
-/*
- We create a new book object, add it to the myBooks array, save the array to local storage,
- and then display the book
- title - The title of the book
- author - The author of the book.
- */
-function addBook(title, author) {
-  const book = new Book(title, author);
-  myBooks.push(book);
-  saveBookToLocalStorage();
-  displayBook();
-}
-
-/*
- If there are books in local storage, parse them and store them in the myBooks array.
- If there are no books in local storage, display a message to the user
- */
-function getBooksFromLocalStorage() {
-  const books = localStorage.getItem('books');
-  if (books) {
-    myBooks = JSON.parse(books);
-  } else {
-    myBooks.innerHTML = 'No books found';
-  }
-}
-// Call the function to retrieve the list of books from local storage
-getBooksFromLocalStorage();
-// Call the displayBook function here to display the list of books on the page.
-displayBook();
-
-/* Adding an event listener to the form that listens for a submit event.
-When the event is triggered, it prevents the form from submitting and reloading the page.
-It then retrieves the title and author input values and adds the book to the list and clears
-the input fields.
-*/
-booksForm.addEventListener('submit', (e) => {
-  // Prevent the form from submitting and reloading the page
+// Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  // Prevent actual submit
   e.preventDefault();
 
-  // Retrieve the title and author input values
-  const titleInput = document.getElementById('title');
-  const authorInput = document.getElementById('author');
+  // Get form values
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
 
-  // Add the book to the list and clear the input fields
-  addBook(titleInput.value, authorInput.value);
-  titleInput.value = '';
-  authorInput.value = '';
+  // Validate
+  if (title === '' || author === '') {
+    const list = document.querySelector('#book-list');
+    const getError = document.getElementById('error');
+    getError.textContent = 'Please fill out all fields';
+  } else {
+    // Add Book to UI
+    const randomId = Math.random() * 1000;
+    const id = Math.floor(randomId);
+    AddBook.addBook(title, author, id);
+    const getError = document.getElementById('error');
+    getError.textContent = '';
+  }
 });
+
+// Event: Remove a Book
+
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    const removeBooks = document.querySelectorAll('.remove-btn');
+    removeBooks.forEach((item) => item.addEventListener('click', (e) => {
+      RemoveBook.removeBook(e.target.parentElement.firstChild.innerHTML);
+    }));
+  },
+);
